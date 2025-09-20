dcb10750299a862611cd8de68dfed247fa3da292
Documenter.jl <documenter@juliadocs.github.io>
2025-09-20 18:46:40 +0000

    build based on 2136627

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DeviceSparseArrays","page":"Home","title":"DeviceSparseArrays","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DeviceSparseArrays.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
